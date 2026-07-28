7dc0ea52998adf620096f6d658feed69d4bf7e1d
Sathvik Kasthuri <kasthurisathvik17@gmail.com>
2026-07-28 13:16:31 +0530

    Build Hero Section UI

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import "./Home.css"
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <Navbar />
 
       <main>
@@ -11,16 +12,19 @@ const Home = () => {
           <h1>Practice. Improve. Speak With Confidence.</h1>
 
           <p>
-            AI-powered platform for interview preparation and daily speaking
-            practice.
+            Improve your English communication and interview skills with instant
+            AI feedback and personalized practice.
           </p>
 
-          <button>Start Practicing</button>
+          <div className="hero-btns">
+            <button className="primary-btn">Start Practicing</button>
+            <button className="secondary-btn">Learn More</button>
+          </div>
         </section>
       </main>
 
       <Footer />
-    </>
+    </div>
   );
 };
 
