c36ff03f72626c3742c983dcd6c780d9ae31186c
Sathvik Kasthuri <kasthurisathvik17@gmail.com>
2026-08-14 11:49:26 +0530

    Build Responsive Landing Page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/AI Logo.png";
-import "./Navbar.css"
+import "./Navbar.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo">
         <img src={logo} />
       </div>
 
-      <ul className="nav-links">
-        <li>Home</li>
-        <li>Features</li>
-        <li>About</li>
-        <li>Contact</li>
+      <button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
+        <FontAwesomeIcon icon={menuOpen ? faXmark : faBars} />
+      </button>
+
+      <ul className={`nav-links ${menuOpen ? "menu-open" : ""}`}>
+        <li onClick={() => setMenuOpen(false)}>Home</li>
+        <li onClick={() => setMenuOpen(false)}>Features</li>
+        <li onClick={() => setMenuOpen(false)}>About</li>
+        <li onClick={() => setMenuOpen(false)}>Contact</li>
       </ul>
 
       <button className="navbar-btn">Get Started</button>
